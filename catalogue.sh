1511b34cc05dc054bf2c1b3f5592e4b6184cd3b2
sravani3093 <sravani.jamithireddy@outlook.com>
2024-05-21 19:10:55 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -26,43 +26,58 @@ else
     echo -e " $G proceed to runthe script $N"
 fi
 
-dnf module disable nodejs -y &>>$LOGFILE
+dnf module disable nodejs -y &>> $LOGFILE
 VALIDATE $? "module disable nodejs"
-dnf module enable nodejs:18 -y &>>$LOGFILE
+
+dnf module enable nodejs:18 -y &>> $LOGFILE
 VALIDATE $? "module enable nodejs:18"
-useradd roboshop &>>$LOGFILE
+
+useradd roboshop &>> $LOGFILE
 VALIDATE $?  "USER ADDED"
-mkdir  -p /app &>>$LOGFILE
+
+mkdir  -p /app &>> $LOGFILE
 VALIDATE $?  "app Directory"
-curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>> $LOGFILE
 VALIDATE $? "Catalogue File Downloaded"
-cd /app &>>$LOGFILE
+
+cd /app &>> $LOGFILE
 VALIDATE $? "Changed the Directoy to  /app"
-unzip /tmp/catalogue.zip &>>$LOGFILE
+
+unzip /tmp/catalogue.zip &>> $LOGFILE
 VALIDATE $? "File Unzip"
-cd /app &>>$LOGFILE
+
+cd /app &>> $LOGFILE
 VALIDATE $? "Changed the Directoy to  /app"
-npm install &>>$LOGFILE
+
+npm install &>> $LOGFILE
 VALIDATE $? "Dependecies Installed"
-cp catalogue.service /etc/systemd/system &>>$LOGFILE
+
+cp catalogue.service /etc/systemd/system &>> $LOGFILE
 VALIDATE $? "File copied"
-systemctl daemon-reload &>>$LOGFILE
+
+systemctl daemon-reload &>> $LOGFILE
 VALIDATE $? "daemon-reload"
-systemctl enable catalogue  &>>$LOGFILE
+
+systemctl enable catalogue  &>> $LOGFILE
 VALIDATE $? "catalogue enabled"
-systemctl start catalogue  &>>$LOGFILE
+
+systemctl start catalogue  &>> $LOGFILE
 VALIDATE $? "catalogue started"
-cp mongo.repo /etc/yum.repos.d &>>$LOGFILE #copying the repo file
+
+cp mongo.repo /etc/yum.repos.d &>> $LOGFILE #copying the repo file
 VALIDATE $? "mongo repo copied "
+
 rpm -qa | grep -i mongodb-org-shell
 if [ $? -ne 0 ]
 then 
-    dnf install mongodb-org-shell -y  &>>$LOGFILE
+    dnf install mongodb-org-shell -y &>> $LOGFILE
     VALIDATE $? "MONGODB INSTALLED"
 else 
     echo -e " MONGODB IS ALREADY INSTALLED ... $Y SKIPPING $N"
 fi
-mongo --host $MONGODB </app/schema/catalogue.js  &>>$LOGFILE
+
+mongo --host $MONGODB </app/schema/catalogue.js &>> $LOGFILE
 VALIDATE $? "catalogue loaded "
 
 
